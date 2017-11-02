c1b50907b3a10e4c766499e0c4be960a34dc6504
Sting XU <sting.xu@anu.edu.au>
2017-11-02 14:09:45 +1100

    Initial Commit

---
+++ synPop/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  synPop
+//
+//  Created by Sting Xu on 2/11/17.
+//  Copyright © 2017 Sting Xu. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
